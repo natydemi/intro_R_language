87ddf13f4705e5b5705f92b3ac331eec9467629b
Nath Dell Desktop <natydemi@gmail.com>
2022-02-02 00:30:46 -0300

    ajuste nomes

---
+++ aulas_scripts/script_aula1.R
@@ -0,0 +1,276 @@
+
+# Introdução à Programação em R
+# Nathália Demetrio
+
+
+# Apresentações  -----
+    #professora e monitoras
+    #revisar forms
+    #combinar dinamica das aulas
+
+# Agenda -----
+  #contextualizar temas
+  #dialetos do R: Rbase, tidyverse, data.table
+
+# Primeiros passos  -----
+
+  # Contextualização
+    # https://www.r-project.org/
+
+  # Da Instalação ao `Hello World`
+    # https://cloud.r-project.org
+
+  # A IDE Rstudio 
+    # https://www.rstudio.com/
+
+  # Alternativa ao Desktop: Rstudio Cloud 
+    #https://rstudio.cloud/
+
+
+# Um tour pelo Rstudio  -----
+    #Console - painel onde os comandos são executados interativamente;  
+    #Source - onde são abertos os scripts do R, que só será enviado para o Console quando executado; 
+    #Environment - listagem dos objetos definidos ao longo da sessão;
+    #Files - acesso às pastas e arquivos do computador;
+    #Help - menu de ajuda e visualização das documentações consultadas;
+    #History - consulta do histórico de comandos;
+    #Packages - gerenciamento das bibliotecas instaladas;
+    #Plots - visualização dos gráficos gerados; e
+    #Viewer - visualização dos relatórios e aplicativos gerados.
+
+
+# R como calculadora  -----
+    #Textos precedidos por # são comentários (note a mudança na cor do texto)
+
+    #Operações
+        #soma (o separador decimal no R é o ponto)
+        1 + 1.5
+
+        #subtração
+        7 - 2 
+        
+        #divisão
+        9 / 3  
+        
+        #multiplicação
+        100 * 10 
+        
+        #potência
+        2^4 ; 2**4  
+        
+        #resto da divisão de 10 por 3
+        10 %% 3 
+        
+        #parte inteira da divisão de 10 por 3
+        10 %/% 3  
+        
+    #O resultado foi precedido por `[1]`, o que faz referência à quantidade de elementos do resultado
+    #spoiler: contadores no R partem do `1`, e não do `0` como muitas outras linguagens 
+        
+    #podemos escrever o código no Editor (Source/R Script), e envia-lo para a execução no Console 
+    #por meio do atalho ‘Ctrl + Enter’ ou pelo botão ‘Run’ do RStudio.
+        
+    #O R permite ampliar as equações mantendo a notação básica de operações algébricas
+        #equação ok
+        (2 * ( 2 * ( 2 * (4-3))))
+        #equação não ok
+        (2 * { 2 * [ 2 * (4-3)]})
+
+        
+# ::::: check-point -------------------------------------------------------------
+    # @s elun@s devem criar um script, 
+    # salva-lo contendo uma mensagem comentada, 
+    # fechar o script e abri-lo novamente
+        
+       
+         
+# Operadores lógicos -----
+        
+    #Podemos também trabalhar com operadores lógicos no R, tendo como saída TRUE ou FALSE.
+        #maior/menor
+        1 < 0 
+        1 > 0 
+        #menor/maior ou igual
+        1 <= 1 ; 1 >= 1 
+        
+        #igual/diferente (igual são com DOIS simbolos seguidos)
+        1 == 1 
+        1 != 1 
+        
+    #No caso de digitarmos um comando incompleto no Console, o R mostrará o símbolo + 
+    #Para iniciar um novo comando, desconsiderando comandos parciais anteriores, basta pressionar a tecla ‘Esc’. 
+        #comando incompleto
+        8 ==
+            
+    #no caso de comandos que o R não reconheça, ele retornará uma mensagem de erro
+        #erro
+        3 % 9
+        
+    #para resgatar algum comando já digitado no Console, você pode utilizar a tecla ‘seta para cima’    
+    
+
+        
+# Entendendo funções  -----
+
+    #Uma função no R é de um conjunto de instruções organizadas visando executar uma tarefa.
+
+    #No R apenas com as funções builtins, podemos fazer desde cálculos aritméticos a gráficos e modelos estatísticos.
+    #Segue uma lista de funções no R:
+        builtins() 
+
+    #O nome de tais funções no R são, em geral, intuitivas
+        #aplicando a função exponencial ao número 1
+        exp(1)
+        #podemos ver o código da função ao digitar a função sem os parênteses
+        exp
+
+    #Uma das funções mais importantes no R é a função `help()`, ou o seu operador equivalente `?` 
+    #temos acesso a: descrição do objeto consultado, possibilidades de alterações, exemplos, etc
+        ?log
+        
+        #por default o log é calculado na base exponencial
+        log(10)
+        
+        #de modo que o resultado não é alterado mesmo se explicitarmos o argumento 'base'
+        log(10, base = exp(1))
+        
+        #mas podemos altera-lo para qualquer número positivo, como a base 10 por exemplo
+        log(10, base = 10)
+        
+    #Note que não foi necessário especificar o nome do primeiro argumento da função em nenhum dos casos (`log(x=10)`).
+        
+    #No caso de consultas que envolvam objetos com caracteres especiais, é necessário que o texto esteja entre crases.
+        ?`?`
+        
+    #Podemos também obter ajuda considerando tópicos de modo mais geral, por meio das funções `help.search()` ou `??`.
+        ??median
+        
+           
+         
+# Criando objetos  -----
+
+    #Objetos no R são elementos que podem ser armazenados em variáveis, como dados e funções
+        
+    #Podemos criar objetos utilizando o sinal de <- ou =
+    #Objetos podem ser nomeados com letras (case sensitive), números, e os símbolos . e _ 
+        #síntaxe: letras minúsculas
+        objeto <- 1 #atribuição da direita para a esquerda (RECOMENDADO)
+        
+        #síntaxe: letras minúsculas com a primeira letra maiúscula
+        Objeto =  2 #atribuição da direita para a esquerda
+        
+        #síntaxe: letras minúsculas, símbolo e número
+        3 -> objeto_1 #atribuição da esquerda para a direita
+     
+    #Atribuição: a recomendação é trabalhar com <- para atribuições de objetos, 
+    # e = para a especificação de parâmetros de funções, e evitar o `->`
+        
+    #Para inserir o símbolo de atribuição `<-` no RStudio podemos utilizar o atalho 'alt + -'.
+        
+    #Ao criar objetos passamos a ter a informação que estes carregam salvos na memória, e podemos chama-los  
+       sqrt(Objeto) 
+        
+    #Podemos fazer operações entre elementos e objetos
+       #atribuindo um elemento diretamente
+       a <- 3; a
+       #resultado de operações entre elementos
+       b <- 5 + a ; b
+       
+       #excluindo o objeto a
+       rm(a); a
+       
+       #operações entre outros objetos
+       c <-  b * (-1); c
+       #resultado da aplicação de funções
+       d <- abs(c); d
+       
+   #Para listar todos os objetos disponíveis na sessão podemos utilizar a função `ls()`
+   #Enquanto que para excluir algum destes objetos temos a função `rm(nome_do_objeto)`
+   
+   # Regras para nomear objetos    
+       #É possível trabalhar com objetos nomeados com sintaxes não padrão, porém é necessário utilizar aspas
+       #definindo o objeto com sintaxe não padrão
+       `1¨ objeto c/ $int@xe que $&ria #inva!ida for@ da$ a$pa$` <- 6 
+       
+       #aplicando-o em uma função (log na base 10)
+       log10(`1¨ objeto c/ $int@xe que $&ria #inva!ida for@ da$ a$pa$`)
+       
+       #no R existem alguns nomes reservados para representar os seguintes casos especiais    
+       #Not Available: uma constante lógica utilizada para representar dados faltantes
+       NA
+       
+       #Not a Number: objeto lógico representando indefinições matemáticas, como log de números negativos.
+       NaN
+       
+       #Infinite: conceito matemático (positivo ou negativo).
+       Inf; -Inf
+       
+       #Null object: representa a ausência de informação, utilizada como retorno de funções cujos valores são indefinidos.
+       NULL
+       
+       #Visto a relevância de tais objetos, estes não podem ser reescritos. Diferentemente de outras constantes
+       NA <- 0
+       
+       pi
+       pi <- 0
+       pi
+
+       
+# ::::: hands-on: criação de objetos -----
+    # @s alun@s devem criar um objeto chamado c, e outro chamado f, em que:
+       # 1) o c irá receber como input uma temperatura em graus celsius, digitado pelo usuário
+       # 2) e o f será uma fórmula que tem o c como um dos seus valores
+       # 3) ao mudar o valor de c o que acontece com o f? é atualizado? 
+       #    Algo precisaria ser feito para ele ser atualizado?
+       # 4) substitua o c para que receba a função readline(prompt="Graus Celsios: "), o que acontece?
+       # 5) digite novamente a definição do objeto f, avalie a mensagem de erro, e 
+       #    investigue o uso das funções class() e as.numeric() para solucionar o problema
+
+
+# ::::: practice: estatísticas descritivas (base mtcars)-----
+    #base para exemplo
+    mtcars
+        
+      View(mtcars)
+     
+      #`head()` - que retorna as primeiras linhas da base de dados, ou similarmente a função `tail()` que contempla as últimas linhas:
+      head(mtcars)
+      tail(mtcars)
+      
+      
+      #`str()` - exibe a estrutura interna de um objeto, no caso da base de dados que estamos trabalhando p.e., temos: a estrutura dos dados (data.frame), o número de linhas (32 observações) e colunas (11 variáveis), além da classe de cada uma das colunas, e uma amostra das primeiras observações de cada uma das colunas: 
+      str(mtcars)
+      
+      
+      # `summary()` - é uma função genérica usada para produzir resumos de resultados segundo várias funções descritivas, no caso de variáveis numéricas, por exemplo: 
+      summary(mtcars)
+                  
+      cor(mtcars)
+       
+      #plot
+      plot(mtcars)
+      
+      #boxplot
+      boxplot(mtcars)
+      
+      #como esta base faz parte do repertório básico do R, 
+      # não a visualizamos na aba `Environment`, salvo se fizermos:
+      data("mtcars")
+
+
+    #para consultar mais funções estatísticas: help(package=stats) 
+
+      #regressão: um exemplo em que ajustamos a variável qsec 
+      # em função da variável carb:
+      fit <- lm(qsec ~ carb, data = mtcars)  
+      summary(fit)
+      plot(fit)
+      
+      #excluindo o modelo
+      rm(fit)
+      
+      # agora a variável qsec em função de todas as demais:
+      fit <- lm(qsec ~ ., data = mtcars)  
+      summary(fit)
+      plot(fit)
+        
---
+++ aulas_scripts/script_aula4.R
@@ -0,0 +1,194 @@
+
+# Introdução à Programação em R
+# Nathália Demetrio
+
+# Workflow da Ciência de Dados -----
+    # https://r4ds.had.co.nz/introduction.html
+
+# o que são dados tidy?  -----
+    #https://escoladedados.org/tutoriais/tidy-data-dados-arrumados-e-5-problemas-comuns/
+
+# o que é um tibble? (vs. data.frame)  -----
+    #https://cran.r-project.org/web/packages/tibble/vignettes/tibble.html
+    
+# O que é o operador pipe?  -----
+    # https://www.curso-r.com/blog/2018-07-03-tutorial-pipe/
+
+# As bibliotecas do tidyverse -----
+    # https://www.tidyverse.org/ 
+
+# ::::: practice: Rbase vs. tidyverse (base titanic) -----
+
+    install.packages("titanic")
+    install.packages("tidyverse")
+    library(tidyverse)
+
+    #dataset
+        (titanic <- titanic::titanic_train)
+    
+        #note como na versão tibble o output é mais clean
+        tibble(titanic)
+        #mas a função que transforma em tibble existe em vários pacotes,
+        #inclusive essa mudança pode ser feita de outras formas, p.e.:
+        titanic <- as_tibble(titanic)
+        # se não tivessemos carregado a biblioteca do tidyverse, ou o dplyr,
+        # a função precisaria ser chamada da seguinte forma:
+        titanic <- dplyr::as_tibble(titanic)
+    
+
+    #Primeiras views
+        #View(titanic)
+        #view(titanic) # msm coisa, mas com letra minuscula
+        titanic %>% view()
+        
+        #str(titanic)
+        titanic %>% glimpse()
+       
+            
+    #atalho para o Pipe: 'ctrl + shift + m'
+
+        #select
+            #titanic[ ,c("Sex", "Age")]
+
+            titanic %>% 
+                #select(Age, Sex)
+                #select(Sex, Age)
+                #select(-Sex)
+                select(1:5)
+            
+         #filter
+            #titanic[titanic$Sex == "male",]
+            #titanic[(titanic$Sex == "male") & (titanic$Age > 30),]
+            
+            titanic %>% 
+                #filter(Sex == "male")
+                filter(Sex == "male", Age > 30 | is.na(Age))  
+
+
+        #arrange
+            #sort(titanic$Sex) ; order(titanic$Sex)
+            #titanic[order(titanic$Sex), ] 
+            
+            titanic %>% 
+                #arrange(Sex, PassengerId) %>% 
+                arrange(Sex, -PassengerId) %>% 
+                arrange(Sex, desc(PassengerId)) %>% 
+                #arrange(desc(Sex)) #com -Sex dá erro, pois ñ faz sentido multiplicar um character por -1
+                filter(Sex == "male")
+
+            #primeiras 5 linhas
+            titanic %>% arrange(-Age) %>% slice(1:5)
+                #top_n e o slice_max
+            
+        #mutate 
+            #titanic$new_col <- titanic$Age * titanic$Survived 
+            
+            titanic %>% 
+                #mutate(Survived = Survived +1 )
+                #mutate(new_col = Age * Survived )
+                mutate(Survived_2 = ifelse(Survived == 1, "sobreviveu", "morreu")) %>% 
+                select(contains("Survived"))
+    
+        #summarise 
+            titanic %>% 
+                summarise(
+                    n = n(),
+                    n_dist_age = n_distinct(Age),
+                    min_age = min(Age, na.rm = T), 
+                    mean_age = mean(Age, na.rm = T), 
+                    median_age = median(Age, na.rm = T),
+                    max_age = max(Age, na.rm = T), 
+                    sd_age = sd(Age, na.rm = T)
+                    )
+            
+        #group_by 
+            titanic %>%
+                #filter(!is.na(Age)) %>% 
+                group_by(Pclass) %>% 
+                summarise(
+                    n = n(),
+                    n_dist_age = n_distinct(Age),
+                    mean_age = mean(Age, na.rm = T), 
+                    sd_age = sd(Age, na.rm = T), 
+                    mean(Fare, na.rm = T)
+                )
+               
+
+            #exemplo com tudo: adicionando porcentagem
+                titanic %>% 
+                    #group_by(Survived, Pclass, Sex) %>%
+                    group_by(Survived) %>%
+                    count() %>% 
+                    # summarise(n = n()) %>% 
+                    ungroup() %>% 
+                    mutate(p = n/sum(n))
+                
+                titanic %>% 
+                    group_by(Survived, Pclass) %>%
+                    count() %>% 
+                    # summarise(n = n()) %>% 
+                    group_by(Survived) %>% 
+                    mutate(p = n/sum(n))
+               
+    # se eu quiser reescrever o objeto com as alterações feitas, 
+    # basta colocar `titanic <- `no início da sequência de comandos
+
+# ::::: practice: explorando dplyr (base iris) -------------------------------------
+
+    data("iris")
+     
+    iris %>% colnames()
+    iris <- iris %>% as_tibble()    
+    
+       
+   iris_tidy <- iris %>% 
+       #arrange(desc(Sepal.Width)) %>% 
+       arrange(-Sepal.Width) %>% 
+        #filter(Sepal.Length > 5 & Sepal.Width < 4) %>% 
+        filter(Sepal.Length > 5 | Sepal.Width < 4) %>%  
+        slice(1:5) %>% 
+        # select(starts_with("Sepal"))
+        # select(contains("."))
+        #select(-Petal.Length) 
+        #select(Sepal.Length, Petal.Width, Species) %>%
+        # select(1:2)
+        rename(var_original = Sepal.Length) %>%
+        mutate(var_normalizada = scale(var_original)) %>% 
+        glimpse()
+   
+   
+   iris_tidy %>% 
+        ggplot(aes(var_normalizada)) +
+            geom_histogram()
+   
+
+# ::::: hands-on: manuseando dados (tidyverse - dplyr) -----
+    #No RStudio Learn Primers: #https://rstudio.cloud/learn/primers 
+    # faça os exercícios propostos na aba `Work with Data`,
+    # priorizando a seção `Derive Information with dplyr`
+    # https://rstudio.cloud/learn/primers/2.3
+                
+# ::::: hands-on: cheatsheet (dplyr) -----
+   #replicar os exemplos da cheatsheet do dplyr
+   #https://github.com/rstudio/cheatsheets/blob/main/data-transformation.pdf
+   
+   
+# ::::: Refs -----
+    #comparação de sintaxe: Rbase vs. tidyverse vs. data.table
+        # https://github.com/mayer79/data_preparation_r
+                  
+    #link cheatsheets no site da rstudio: 
+    #note que ao fim da página você encontrará algumas cheatsheets em português
+        # https://rstudio.com/resources/cheatsheets/
+
+    #livros
+        #(inglês)
+        # R for Data Science, by by Garrett Grolemund, Hadley Wickham:
+        # https://r4ds.had.co.nz/ 
+           
+        #(português)
+        # Curso-R ~ material em português
+        # https://livro.curso-r.com/
+       
+    
+               
